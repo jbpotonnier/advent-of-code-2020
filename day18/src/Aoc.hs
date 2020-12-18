3a8f550a6c036a414e393b4bd1f4e6d9239bd114
Jean-Baptiste Potonnier <jean.baptiste.potonnier@gmail.com>
2020-12-18 14:18:23 +0100

    refacto

--- day18/src/Aoc.hs
+++ day18/src/Aoc.hs
@@ -16,41 +16,23 @@ data Exp
 
 type Parser = Parsec Void Text
 
-compute :: Text -> Maybe Int
-compute = fmap eval . parseExp
-
-compute2 :: Text -> Maybe Int
-compute2 = fmap eval . parseExp2
-
 eval :: Exp -> Int
 eval = \case
   (Add a b) -> eval a + eval b
   (Mult a b) -> eval a * eval b
   (Number n) -> n
 
-parseExp :: Text -> Maybe Exp
-parseExp = parseMaybe expP
-
-parseExp2 :: Text -> Maybe Exp
-parseExp2 = parseMaybe expP2
-
-termP :: Parser Exp
-termP = choice [parens expP, numberP]
-
-termP2 :: Parser Exp
-termP2 = choice [parens expP2, numberP]
-
 expP :: Parser Exp
 expP = makeExprParser termP operatorTable
+  where
+    operatorTable = [[binary "*" Mult, binary "+" Add]]
+    termP = choice [parens expP, numberP]
 
-expP2 :: Parser Exp
-expP2 = makeExprParser termP2 operatorTable2
-
-operatorTable :: [[Operator Parser Exp]]
-operatorTable = [[binary "*" Mult, binary "+" Add]]
-
-operatorTable2 :: [[Operator Parser Exp]]
-operatorTable2 = [[binary "+" Add], [binary "*" Mult]]
+exp2P :: Parser Exp
+exp2P = makeExprParser termP operatorTable
+  where
+    operatorTable = [[binary "+" Add], [binary "*" Mult]]
+    termP = choice [parens exp2P, numberP]
 
 binary :: Text -> (a -> a -> a) -> Operator (ParsecT Void Text Identity) a
 binary name f = InfixL (f <$ symbol name)
@@ -77,60 +59,5 @@ lexeme = Lexer.lexeme spaceConsumer
 symbol :: Text -> Parser Text
 symbol = Lexer.symbol spaceConsumer
 
-readInput :: MonadIO f => FilePath -> f [Exp]
-readInput path = mapMaybe parseExp . lines <$> readFileText path
-
-readInput2 :: MonadIO f => FilePath -> f [Exp]
-readInput2 path = mapMaybe parseExp2 . lines <$> readFileText path
-
------------------------------------
-
--- converge :: Eq t => (t -> t) -> t -> t
--- converge f x = let res = f x in if res == x then res else converge f res
-
--- applyTimes :: (c -> c) -> Int -> c -> c
--- applyTimes f n = (!! n) . iterate f
-
--- count :: (Ord k, Foldable t) => t k -> k -> Int
--- count xs x = fromMaybe 0 (c IMap.!? x)
---   where
---     c = IMap.fromListWith (+) . fmap (,1) . toList $ xs
-
--- readInt :: Text -> Int
--- readInt = fromJust . readMaybe . toString
-
--- head' :: [c] -> c
--- head' = fromJust . viaNonEmpty head
-
--- headMay :: [b] -> Maybe b
--- headMay = viaNonEmpty head
-
--- tail' :: [a] -> [a]
-
--- search :: Eq a => Vector (Vector a) -> Vector (Vector a)
--- search vs
---  | isSolution vs = vs
---  | otherwise =
---    let candidateIndex = V.minIndexBy (comparing V.length) vs
---     in search . eliminate $ assign vs candidateIndex
-
--- assign :: Vector (Vector a) -> Int -> Vector (Vector a)
--- assign vs i = vs V.// [(i, V.singleton (V.head (vs V.! i)))]
-
--- solutionAsList :: Vector (Vector a) -> [a]
--- solutionAsList = toList . V.map V.head
-
--- isSolution :: Vector (Vector a) -> Bool
--- isSolution = all (\v -> V.length v == 1)
-
--- eliminate :: Eq a => Vector (Vector a) -> Vector (Vector a)
--- eliminate v = foldl' eliminateAt v [0 .. V.length v - 1]
-
--- eliminateAt :: Eq a => Vector (Vector a) -> Int -> Vector (Vector a)
--- eliminateAt vs i
---   | V.length cur == 1 =
---     let val = V.head cur
---      in V.imap (\j v -> if i /= j then V.filter (/= val) v else v) vs
---   | otherwise = vs
---   where
---     cur = vs V.! i
+readInput :: (MonadIO f) => Parser b -> FilePath -> f [b]
+readInput p path = mapMaybe (parseMaybe p) . lines <$> readFileText path
--- day18/test/Spec.hs
+++ day18/test/Spec.hs
@@ -1,17 +1,17 @@
 module Main (main) where
 
-import Aoc
-import Test.Hspec
-import Test.Hspec.Megaparsec
-import Text.Megaparsec (ParseErrorBundle, Parsec, parse)
+import Aoc ( Parser, Exp(..), eval, expP, readInput, exp2P )
+import Test.Hspec ( hspec, describe, it, shouldBe )
+import Test.Hspec.Megaparsec ( shouldParse )
+import Text.Megaparsec (ParseErrorBundle, Parsec, parse, parseMaybe)
 
 main :: IO ()
 main = hspec $ do
   describe "Parser" $ do
-    it "add" $ do
+    it "add" $
       parseText expP "12 + 14" `shouldParse` Add (Number 12) (Number 14)
 
-    it "mult" $ do
+    it "mult" $
       parseText expP "2 * 5" `shouldParse` Mult (Number 2) (Number 5)
 
     it "parens" $ do
@@ -28,27 +28,30 @@ main = hspec $ do
 
   describe "Aoc" $ do
     it "examples" $ do
-      compute "2 * 3 + (4 * 5)" `shouldBe` Just 26
-      compute "5 + (8 * 3 + 9 + 3 * 4 * 3)" `shouldBe` Just 437
-      compute "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" `shouldBe` Just 12240
-      compute "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" `shouldBe` Just 13632
+      evalWithParser expP "2 * 3 + (4 * 5)" `shouldBe` Just 26
+      evalWithParser expP "5 + (8 * 3 + 9 + 3 * 4 * 3)" `shouldBe` Just 437
+      evalWithParser expP "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" `shouldBe` Just 12240
+      evalWithParser expP "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" `shouldBe` Just 13632
 
     it "star 1" $ do
-      exps <- readInput "./test/input.txt"
+      exps <- readInput expP "./test/input.txt"
       length exps `shouldBe` 374
       (sum . fmap eval) exps `shouldBe` 67800526776934
 
     it "example 2 " $ do
-      compute2 "1 + (2 * 3) + (4 * (5 + 6))" `shouldBe` Just 51
-      compute2 "2 * 3 + (4 * 5)" `shouldBe` Just 46
-      compute2 "5 + (8 * 3 + 9 + 3 * 4 * 3)" `shouldBe` Just 1445
-      compute2 "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" `shouldBe` Just 669060
-      compute2 "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" `shouldBe` Just 23340
+      evalWithParser exp2P "1 + (2 * 3) + (4 * (5 + 6))" `shouldBe` Just 51
+      evalWithParser exp2P "2 * 3 + (4 * 5)" `shouldBe` Just 46
+      evalWithParser exp2P "5 + (8 * 3 + 9 + 3 * 4 * 3)" `shouldBe` Just 1445
+      evalWithParser exp2P "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" `shouldBe` Just 669060
+      evalWithParser exp2P "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" `shouldBe` Just 23340
 
     it "star 2" $ do
-      exps <- readInput2 "./test/input.txt"
+      exps <- readInput exp2P "./test/input.txt"
       length exps `shouldBe` 374
       (sum . fmap eval) exps `shouldBe` 340789638435483
 
 parseText :: Parsec e s a -> s -> Either (ParseErrorBundle s e) a
 parseText p = parse p "inputText"
+
+evalWithParser :: Parser Exp -> Text -> Maybe Int
+evalWithParser p = fmap eval . parseMaybe p
