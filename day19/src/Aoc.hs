ae8fe9051ad74c9d26ecdf29c31cce6cf7d8a575
Jean-Baptiste Potonnier <jean.baptiste.potonnier@gmail.com>
2020-12-19 20:15:49 +0100

    clean

--- day19/src/Aoc.hs
+++ day19/src/Aoc.hs
@@ -105,52 +105,5 @@ splitOn s t = case T.splitOn s t of
   [a, b] -> (a, b)
   _ -> error $ "splitOn: cannot split " <> show t <> " on " <> show s
 
--- converge :: Eq t => (t -> t) -> t -> t
--- converge f x = let res = f x in if res == x then res else converge f res
-
--- applyTimes :: (c -> c) -> Int -> c -> c
--- applyTimes f n = (!! n) . iterate f
-
--- count :: (Ord k, Foldable t) => t k -> k -> Int
--- count xs x = fromMaybe 0 (c IMap.!? x)
---   where
---     c = IMap.fromListWith (+) . fmap (,1) . toList $ xs
-
 read' :: Read a => Text -> a
 read' = fromJust . readMaybe . toString
-
--- head' :: [c] -> c
--- head' = fromJust . viaNonEmpty head
-
--- headMay :: [b] -> Maybe b
--- headMay = viaNonEmpty head
-
--- tail' :: [a] -> [a]
-
--- search :: Eq a => Vector (Vector a) -> Vector (Vector a)
--- search vs
---  | isSolution vs = vs
---  | otherwise =
---    let candidateIndex = V.minIndexBy (comparing V.length) vs
---     in search . eliminate $ assign vs candidateIndex
-
--- assign :: Vector (Vector a) -> Int -> Vector (Vector a)
--- assign vs i = vs V.// [(i, V.singleton (V.head (vs V.! i)))]
-
--- solutionAsList :: Vector (Vector a) -> [a]
--- solutionAsList = toList . V.map V.head
-
--- isSolution :: Vector (Vector a) -> Bool
--- isSolution = all (\v -> V.length v == 1)
-
--- eliminate :: Eq a => Vector (Vector a) -> Vector (Vector a)
--- eliminate v = foldl' eliminateAt v [0 .. V.length v - 1]
-
--- eliminateAt :: Eq a => Vector (Vector a) -> Int -> Vector (Vector a)
--- eliminateAt vs i
---   | V.length cur == 1 =
---     let val = V.head cur
---      in V.imap (\j v -> if i /= j then V.filter (/= val) v else v) vs
---   | otherwise = vs
---   where
---     cur = vs V.! i
