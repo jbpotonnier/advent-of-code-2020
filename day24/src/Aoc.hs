ab4aa5c1f8e164a503bd0621f657eabd7d64901a
Jean-Baptiste Potonnier <jean.baptiste.potonnier@gmail.com>
2020-12-24 17:20:45 +0100

    star 2

--- day24/src/Aoc.hs
+++ day24/src/Aoc.hs
@@ -3,10 +3,54 @@ module Aoc
   )
 where
 
+import Data.Foldable (Foldable (maximum, minimum))
 import qualified Data.Map.Strict as Map
+import Data.Set (intersection)
+import qualified Data.Set as Set
 
-blackTiles :: [(Int, Int, Int)] -> [(Int, Int, Int)]
-blackTiles = fmap fst . filter (odd . snd) . Map.toList . count
+applyRules :: Set (Int, Int, Int) -> Set (Int, Int, Int)
+applyRules ts = Set.filter nextIsBlack . allTiles $ ts
+  where
+    nextIsBlack t
+      | isBlack t =
+        let blackCount = countBlackNeibors t
+         in not (blackCount == 0 || blackCount > 2)
+      | otherwise = countBlackNeibors t == 2
+
+    countBlackNeibors t = Set.size $ neighbors t `intersection` ts
+
+    isBlack t = Set.member t ts
+
+neighbors :: (Int, Int, Int) -> Set (Int, Int, Int)
+neighbors c =
+  fromList . fmap (add c) $
+    [(-1, 0, 1), (0, -1, 1), (1, 0, -1), (0, 1, -1), (1, -1, 0), (-1, 1, 0)]
+
+allTiles :: Set (Int, Int, Int) -> Set (Int, Int, Int)
+allTiles ls =
+  fromList
+    [ (x, y, z)
+      | x <- [minX .. maxX],
+        y <- [minY .. maxY],
+        z <- [minZ .. maxZ],
+        x + y + z == 0
+    ]
+  where
+    minX = (\x -> x - 1) . minimum . Set.map getX $ ls
+    maxX = (+ 1) . maximum . Set.map getX $ ls
+
+    minY = (\x -> x - 1) . minimum . Set.map getY $ ls
+    maxY = (+ 1) . maximum . Set.map getY $ ls
+
+    minZ = (\x -> x - 1) . minimum . Set.map getZ $ ls
+    maxZ = (+ 1) . maximum . Set.map getZ $ ls
+
+    getX (x, _, _) = x
+    getY (_, y, _) = y
+    getZ (_, _, z) = z
+
+blackTiles :: [(Int, Int, Int)] -> Set (Int, Int, Int)
+blackTiles = fromList . fmap fst . filter (odd . snd) . Map.toList . count
 
 add :: (Num a, Num b, Num c) => (a, b, c) -> (a, b, c) -> (a, b, c)
 add (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
--- day24/test/Spec.hs
+++ day24/test/Spec.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
 import Aoc
+import Data.List ((!!))
+import qualified Data.Set as Set
 import Test.Hspec
 
 main :: IO ()
@@ -25,3 +27,15 @@ main = hspec $ do
 
       (length . blackTiles <$> readInput "./test/input.txt")
         `shouldReturn` 322
+
+    it "star 2" $ do
+      star2 <$> readInput "./test/example.txt" `shouldReturn` 2208
+      star2 <$> readInput "./test/input.txt" `shouldReturn` 3831
+
+star2 :: [(Int, Int, Int)] -> Int
+star2 =
+  (!! 100)
+    . fmap Set.size
+    . iterate applyRules
+    . blackTiles
+    . fromList
